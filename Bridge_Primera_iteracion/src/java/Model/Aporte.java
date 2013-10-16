48729d2228a0da62acace8c1f78d7f418deedfb6
DianaRosas <DianaRosas@ciencias.unam.mx>
2013-10-16 11:23:36 -0500

    Update Aporte.java

--- Bridge_Primera_iteracion/src/java/Model/Aporte.java
+++ Bridge_Primera_iteracion/src/java/Model/Aporte.java
@@ -4,10 +4,68 @@
  */
 package Model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
- *
- * @author Javier
+ *Clase principal que se encarga de la administracion de un Aporte
+ * @author drosas
+ * @version 1.0 14/10/13
  */
+
 public class Aporte {
+    Conexion cn;
     
+    public Aporte(){
+        cn = new Conexion();
+    }
+    
+    
+    public boolean newAporte(Integer idCreativo, Integer idProblema, String titulo, String descripcion,Integer costo, String fecha){
+        //Validaciones
+                boolean estado;
+        estado = false;
+        try {
+         Connection con = cn.getConexion();
+         // Instrucci�n SQL para obtener los datos
+         // del usuario indicado :
+         String query =  "INSERT INTO aporte(idAporte,titulo,descripcion,costo,fecha,idProblemaFK,idCreativoFK) VALUES("+titulo+","+descripcion+","+costo+","+fecha+","+idCreativo+","+idProblema+")";
+         Statement st = con.createStatement();
+         ResultSet rs = st.executeQuery( query );
+         estado = true;
+         cn.cierraConexion( con );
+        }
+         catch( Exception e ) {
+             e.printStackTrace();
+         }
+        finally {
+            return estado;
+        }
+    }
+    
+    //
+    
+    public boolean updateAporte(Integer idCreativo, Integer idProblema, String titulo, String descripcion,Integer costo, String fecha){
+        //Validaciones
+                boolean estado;
+        estado = false;
+        try {
+         Connection con = cn.getConexion();
+         // Instrucci�n SQL para obtener los datos
+         // del usuario indicado :
+         String query =  "INSERT INTO aporte(idAporte,titulo,descripcion,costo,fecha,idProblemaFK,idCreativoFK) VALUES("+titulo+","+descripcion+","+costo+","+fecha+","+idCreativo+","+idProblema+")";
+         Statement st = con.createStatement();
+         ResultSet rs = st.executeQuery( query );
+         estado = true;
+         cn.cierraConexion( con );
+        }
+         catch( Exception e ) {
+             e.printStackTrace();
+         }
+        finally {
+            return estado;
+        }
+    }
+
 }
